4d15481f99e859b03c009554bc2e976a22e17880
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-02-25 14:07:12 +0100

    Add FLY default animation

--- src/main/java/net/pixeldream/valormobs/entity/constant/DefaultAnimations.java
+++ src/main/java/net/pixeldream/valormobs/entity/constant/DefaultAnimations.java
@@ -6,6 +6,7 @@ import mod.azure.azurelib.core.animation.RawAnimation;
 public class DefaultAnimations {
     public static final RawAnimation IDLE = RawAnimation.begin().thenLoop("idle");
     public static final RawAnimation WALK = RawAnimation.begin().thenLoop("walk");
+    public static final RawAnimation FLY = RawAnimation.begin().thenLoop("fly");
     public static final RawAnimation RUN = RawAnimation.begin().thenLoop("run");
     public static final RawAnimation DEATH = RawAnimation.begin().thenPlayAndHold("death");
     public static final RawAnimation ATTACK = RawAnimation.begin().then("attack", Animation.LoopType.PLAY_ONCE);
